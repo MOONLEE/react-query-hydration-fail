5e467ca8cb4d43c4c2ba06038d385ef11628adc6
MOONLEE <lux0425@gmail.com>
2023-11-29 10:10:46 +0900

    add recoil state in view.tsx

--- app/view.tsx
+++ app/view.tsx
@@ -1,11 +1,22 @@
 'use client'
 
 import MainCard from "@/components/MainCard";
+import { homeAtom } from "@/state/state";
+import { useRecoilState } from "recoil";
 
 export default function View() {
 
+    const [state, setState] = useRecoilState(homeAtom);
+
+
     return (
         <>
+            {state.tab === 'a' ?
+            <div>tab a</div>
+            :
+            <div>tab b</div>
+            }
+
             <MainCard />
         </>
     )
